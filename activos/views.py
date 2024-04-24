1cd8e760e212eac9e9d3dce6b4b8de40937207d4
UlisesHuesca <ulises_huesc@hotmail.com>
2024-04-24 14:06:14 -0600

    Todos los cambios a activos

--- activos/views.py
+++ activos/views.py
@@ -54,6 +54,7 @@ def activos(request):
 
 @login_required(login_url='user-login')
 def add_activo(request):
+    #perfil = Profile.objects.get(staff__id=request.user.id)
     pk_perfil = request.session.get('selected_profile_id') 
     perfil = Profile.objects.get(id = pk_perfil)
     #activos = Activo.objects.filter(completo=True)
