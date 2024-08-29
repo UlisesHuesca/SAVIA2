ff5184b775868d9235332dd796269df9f1f23d4d
UlisesHuesca <ulises_huesc@hotmail.com>
2024-08-29 15:07:41 -0600

    Se construye filtro de slicitado para en viáticos

--- viaticos/filters.py
+++ viaticos/filters.py
@@ -8,6 +8,7 @@ class Solicitud_Viatico_Filter(django_filters.FilterSet):
     #staff = CharFilter(field_name='staff__staff', lookup_expr='icontains')
     staff = CharFilter(method ='my_filter', label="Search")
     folio = CharFilter(field_name='folio', lookup_expr='icontains')
+    solicitado = CharFilter(method = 'solicitado_para', label="Search")
     proyecto = CharFilter(field_name='proyecto__nombre', lookup_expr='icontains')
     subproyecto = CharFilter(field_name='subproyecto__nombre', lookup_expr='icontains')
     start_date = DateFilter(field_name ='created_at', lookup_expr='gte')
@@ -19,3 +20,6 @@ class Solicitud_Viatico_Filter(django_filters.FilterSet):
     
     def my_filter(self, queryset,name, value):
         return queryset.filter(Q(staff__staff__staff__first_name__icontains = value) | Q(staff__staff__staff__last_name__icontains = value))
+    
+    def solicitado_para(self, queryset, name, value):
+        return queryset.filter(Q(colaborador__staff__staff__first_name__icontains = value) | Q(colaborador__staff__staff__last_name__icontains = value))
