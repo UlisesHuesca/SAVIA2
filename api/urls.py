3fabd03b8c4bcf80a97a769e2eeaf8f535c8348d
UlisesHuesca <ulises_huesc@hotmail.com>
2024-10-10 13:15:27 -0600

    Manufactura de varios endpoints para finanzas

--- api/urls.py
+++ api/urls.py
@@ -8,9 +8,11 @@ urlpatterns = [
     path('inventario', views.getData, name='api'),
     path('compras', views.CompraAPI, name ='compras'),
     path('proveedores', views.proveedores_api, name="proveedores"),
+    path('proyectos', views.proyectos_api, name="proyectos"),
+    path('subproyectos', views.proyectos_api, name="subproyectos"),
+    path('monedas', views.monedas_api, name="monedas"),
+    path('profiles', views.profiles_api, name="profiles"),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('perfiles_rh/', views.obtener_perfiles, name='perfiles_rh'),
     path('oc-pdf/<int:pk>/', views.descargar_pdf_oc, name='api-oc-pdf'),
-
-
     ]
