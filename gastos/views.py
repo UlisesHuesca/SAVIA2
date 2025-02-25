5064152305eee85ded92cc700dedb0f837619c0f
UlisesHuesca <ulises_huesc@hotmail.com>
2025-02-25 07:01:17 -0600

    Gastos se ponen los superintendentes que son quienes tienes el permiso de supervisor en el caso exclusivo de Brasil

--- gastos/views.py
+++ gastos/views.py
@@ -147,6 +147,8 @@ def crear_gasto(request):
             superintendentes = colaborador.filter(tipo__dg = True, distritos = usuario.distritos, st_activo =True) 
         else:    
             superintendentes = colaborador.filter(tipo__subdirector = True, distritos = usuario.distritos, st_activo =True, sustituto__isnull = True) 
+    elif usuario.distritos.nombre == "BRASIL":
+        superintendentes = colaborador.filter(distritos=usuario.distritos, tipo__supervisor = True, st_activo = True).exclude(tipo__nombre="Admin")
     elif usuario.tipo.superintendente and not usuario.tipo.nombre == "Admin" and not usuario.tipo.nombre == "GERENCIA":
         superintendentes = colaborador.filter(staff =  usuario.staff, distritos = usuario.distritos )  
     else:
