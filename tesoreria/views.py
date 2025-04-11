2371a5cebfee3f85b00de5c37ff5d912b931fce5
UlisesHuesca <ulises_huesc@hotmail.com>
2025-04-10 18:42:12 -0600

    Se le pone el loading indicator a la descarga de facturas

--- tesoreria/views.py
+++ tesoreria/views.py
@@ -1104,6 +1104,7 @@ def matriz_pagos(request):
             zip_buffer.seek(0)
             response = HttpResponse(zip_buffer, content_type='application/zip')
             response['Content-Disposition'] = 'attachment; filename=facturas.zip'
+            response.set_cookie('descarga_iniciada', 'true') # Establecer la cookie para indicar que la descarga ha comenzado
             return response
     for pago in pagos_list:
         if pago.total_facturas == 0:
