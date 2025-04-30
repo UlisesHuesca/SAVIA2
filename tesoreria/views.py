77dbbf6e9f509f1c837def41c30cbf6935ebdaf1
ulises.huesca <ulises.huesc@hotmail.com>
2025-04-29 19:00:27 -0600

    Se integra el response

--- tesoreria/views.py
+++ tesoreria/views.py
@@ -1146,6 +1146,7 @@ def matriz_pagos(request):
 
                 zip_buffer.seek(0)
                 response = HttpResponse(zip_buffer, content_type='application/zip')
+                response.set_cookie('descarga_iniciada', 'true', max_age=20)
                 response['Content-Disposition'] = 'attachment; filename=facturas.zip'
                 return response
     for pago in pagos_list:
