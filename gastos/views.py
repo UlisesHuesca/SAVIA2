ac46fba48de62a7339722b8a419bcc2ea3d3e293
ulises.huesca <ulises.huesc@hotmail.com>
2025-05-22 19:14:06 -0600

    Se ponen vales en la autorizacion solo que vengan de vales autorizados

--- gastos/views.py
+++ gastos/views.py
@@ -907,7 +907,7 @@ def vales_rosa_pendientes_autorizar(request):
     #vales_rosa = ValeRosa.objects.filter(esta_aprobado = None, gasto__superintendente = perfil).order_by('-gasto__folio')
     #print(perfil.tipo.nombre)
     if perfil.tipo.nombre == "Admin": #Temporalmente hasta que este listo el desarrollo
-        vales_rosa = ValeRosa.objects.filter(esta_aprobado = None, gasto__complete = True  ).order_by('-gasto__folio')
+        vales_rosa = ValeRosa.objects.filter(esta_aprobado = None, gasto__complete = True, gasto__autorizar2 = True).order_by('-gasto__folio')
     else:
         vales_rosa = ValeRosa.objects.none()
     
