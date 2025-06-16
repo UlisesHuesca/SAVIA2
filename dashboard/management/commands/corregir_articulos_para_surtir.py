37a2600ca0f3ca582ee9a428bd0b6290d01cb49c
UlisesHuesca <ulises_huesc@hotmail.com>
2025-06-16 09:47:53 -0600

    Se corrigen errores en PROD que hacían que crecieran demasiado los logs de pagos y de articulos para surtir

--- dashboard/management/commands/corregir_articulos_para_surtir.py
+++ dashboard/management/commands/corregir_articulos_para_surtir.py
@@ -1,10 +1,8 @@
 from django.core.management.base import BaseCommand
 from requisiciones.models import Salidas
-#from dashboard.models import ArticulosparaSurtir  # Ajusta el import
+# from dashboard.models import ArticulosparaSurtir  # Ajusta el import si es necesario
 from django.db.models import Sum
 from decimal import Decimal
-from django.utils import timezone
-from datetime import datetime
 import logging
 
 logger = logging.getLogger(__name__)
@@ -15,12 +13,13 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         total_salidas = 0
         modificados = 0
-        fecha_inicio = datetime(2024, 1, 1, tzinfo=timezone.utc)
-        salidas = Salidas.objects.filter(cancelada=False, complete=True, created_at__gte=fecha_inicio)
+
+        # Recupera todas las salidas completadas y no canceladas
+        salidas = Salidas.objects.filter(cancelada=False, complete=True)
 
         for salida in salidas:
             total_salidas += 1
-            articulo = salida.producto #Este es el articulos para surtir relacionado a la salida
+            articulo = salida.producto  # FK hacia ArticulosparaSurtir
 
             if not articulo or articulo.salida:
                 continue  # Ya corregido o producto inválido
@@ -46,4 +45,3 @@ class Command(BaseCommand):
 
         self.stdout.write(f"Evaluadas: {total_salidas} salidas completadas.")
         self.stdout.write(f"Artículos corregidos: {modificados}")
-
