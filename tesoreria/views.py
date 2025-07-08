397590d975d732070164e7a66f286d27012790af
ulises.huesca <ulises.huesc@hotmail.com>
2025-07-08 11:48:45 -0600

    Se repara nombre del field para la descarga correcta de los viáticos en control de documentos v2

--- tesoreria/views.py
+++ tesoreria/views.py
@@ -1729,7 +1729,7 @@ def control_documentos(request):
                                     gen_path = f"GENERAL_XMLs/{factura.id}_{uuid}.xml"
                                     zip_file.write(factura.factura_xml.path, gen_path)
                                     datos_xml_lista.append(extraer_datos_xml_carpetas(factura.factura_xml.path, f"V{viatico.folio}", fecha_subida, viatico.distrito.nombre, beneficiario, gen_path, factura))
-                                    if not factura.archivo_pdf or not os.path.exists(factura.archivo_pdf.path):
+                                    if not factura.factura_pdf or not os.path.exists(factura.factura_pdf.path):
                                         # Si no hay PDF, generamos uno
                                         ruta_pdf = generar_cfdi_viaticos(factura.id)
                                         zip_file.write(ruta_pdf, os.path.join(carpeta, os.path.basename(ruta_pdf)))
