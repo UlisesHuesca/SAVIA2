8cf9df85410a292a15c8f182de90ecad9f5f489d
ulises.huesca <ulises.huesc@hotmail.com>
2025-10-28 14:14:54 -0600

    Se comenta el url de productos

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
-    path('productos_api', views.productos_api, name='productos-api'),
+    #path('productos_api', views.productos_api, name='productos-api'),
     path('inventario_api', views.inventario_api, name='inventario-api'),
     path('solicitudes_api', views.solicitudes_api, name ='solicitudes-api'),
     path('productos_solicitados_api', views.productos_solicitados_api, name ='productos-solicitados-api'),
