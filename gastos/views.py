f65529d6c7615c5d6ca57e0436e4371ca928f415
ulises.huesca <ulises.huesc@hotmail.com>
2026-01-22 14:40:21 -0600

    Se mejora para que la sumatoria del reporte de gastos sea solo lo restante

--- gastos/views.py
+++ gastos/views.py
@@ -2959,7 +2959,7 @@ def convert_excel_gasto_matriz(gastos):
 
     # Asumiendo que las filas de datos comienzan en la fila 2 y terminan en row_num
     ws.cell(row=3, column=columna_max + 1, value=f"=COUNTA(A:A)-1").style = body_style
-    ws.cell(row=4, column=columna_max + 1, value=f"=SUM(I:I)").style = money_resumen_style
+    ws.cell(row=4, column=columna_max + 1, value=f"=SUM(K:K)").style = money_resumen_style
   
 
    
