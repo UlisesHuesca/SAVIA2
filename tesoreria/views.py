449323e8b660acd0b29336a1cac89231dab6960f
ulises.huesca <ulises.huesc@hotmail.com>
2026-02-25 16:45:04 -0600

    Se repara error en el comentario pago

--- tesoreria/views.py
+++ tesoreria/views.py
@@ -5739,6 +5739,8 @@ def convert_excel_control_bancos(cuenta_id, pagos, saldo_inicial_objeto,  start_
 
         if pago.comentario:
             comentario_pago = pago.comentario
+        else:
+            comentario_pago = 'NO HAY COMENTARIOS DISPONIBLES'
 
         distrito = pago.oc.req.orden.distrito.nombre if hasattr(pago, 'oc') and pago.oc else (pago.gasto.distrito.nombre if hasattr(pago, 'gasto') and pago.gasto else (pago.viatico.subproyecto.nombre if hasattr(pago, 'viatico') and pago.viatico else (pago.distrito.nombre if pago.distrito else '')))
         cargo = ''
