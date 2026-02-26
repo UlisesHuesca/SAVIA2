720b7219ae579919257c529ef3c8d10259538de8
ulises.huesca <ulises.huesc@hotmail.com>
2026-02-26 17:00:24 -0600

    Reparando rentabilidad.Se agrega list display al admin de solicitud costos

--- rentabilidad/admin.py
+++ rentabilidad/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from .models import Concepto, Tipo_Costo, Costos, Ingresos, Depreciaciones, Solicitud_Costos, Solicitud_Ingresos
 
+class Solicitud_Costos_Admin(admin.ModelAdmin):
+    list_display = ('id','contrato','distrito','tipo','fecha','complete')
+    #raw_id_fields = ('staff','colaborador','superintendente','proveedor','autorizado_por2') 
+    #search_fields = ('folio','id','staff__staff__staff__first_name')
+
 # Register your models here.
 admin.site.register(Concepto)
 
 admin.site.register(Tipo_Costo)
 
-admin.site.register(Solicitud_Costos)
+admin.site.register(Solicitud_Costos, Solicitud_Costos_Admin)
 
 admin.site.register(Costos)
 
