f0d0094f9d5cc044344dfd8f2b6069607c7abec2
ulises.huesca <ulises.huesc@hotmail.com>
2026-03-19 13:14:55 -0600

    Se repara el campo contrato del filtro de costos de rentabilidad

--- rentabilidad/filters.py
+++ rentabilidad/filters.py
@@ -3,7 +3,7 @@ from . import models
 
 class Costos_Form(django_filters.FilterSet):
     distrito = django_filters.CharFilter(field_name='distrito__nombre', lookup_expr='icontains')
-    contrato = django_filters.CharFilter(field_name='contrato__folio', lookup_expr='icontains')
+    contrato = django_filters.CharFilter(field_name='contrato__nombre', lookup_expr='icontains')
     tipo = django_filters.CharFilter(field_name='tipo__nombre', lookup_expr='icontains')
     fecha = django_filters.DateFilter(field_name='fecha', lookup_expr='icontains')
 
