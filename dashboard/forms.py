ba56c973cf2b47d831d0b10ce6e4039180dc8647
ulises.huesca <ulises.huesc@hotmail.com>
2026-04-09 18:22:38 -0600

    Se agrega el campo clase a los formularios de los proyectos--edición y agregación

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -162,13 +162,13 @@ class Contrato_form(forms.ModelForm):
 class Proyectos_Form(forms.ModelForm):
     class Meta:
         model = Proyecto
-        fields = ['descripcion','nombre','status_de_entrega','contrato'] #Se retiran campos 'cliente','factura','fecha_factura','folio_cotizacion','oc_cliente','activo',
+        fields = ['descripcion','nombre','status_de_entrega','contrato','clase'] #Se retiran campos 'cliente','factura','fecha_factura','folio_cotizacion','oc_cliente','activo',
 
 
 class Proyectos_Add_Form(forms.ModelForm):
     class Meta:
         model = Proyecto
-        fields = ['descripcion','nombre','status_de_entrega','contrato'] #'cliente','factura','fecha_factura','folio_cotizacion','oc_cliente',
+        fields = ['descripcion','nombre','status_de_entrega','contrato','clase'] #'cliente','factura','fecha_factura','folio_cotizacion','oc_cliente',
 
 class Subproyectos_Add_Form(forms.ModelForm):
     class Meta:
