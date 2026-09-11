5287d764af3c41984c001c29b4db8b7dea6af1aa
Ulises Huesca <ulises_huesc@hotmail.com>
2026-09-11 17:45:11 -0600

    Configura dominio Yerod en producción

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['grupovordcab.cloud','www.grupovordcab.cloud','localhost', '127.0.0.1','vordcab.local','yerod.local']
+ALLOWED_HOSTS = ['grupovordcab.cloud','www.grupovordcab.cloud','localhost', '127.0.0.1','yerod.cloud','www.yerod.cloud',]
 
 
 # Application definition
